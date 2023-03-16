07f420bec55e1f1bfb357fe3ecab937c6d5a1be5
vincenzopicone <vincenzopicone88@icloud.com>
2023-03-16 17:29:38 +0100

    final

--- src/EsercizioDipendenti/Main.java
+++ src/EsercizioDipendenti/Main.java
@@ -7,12 +7,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Dipendente D1 = new Dipendente("Matr", Dipartimento.PRODUZIONE );
-
+        Dipendente.stampaDatiDipendente();
         Dipendente.promuovi(D1);
         Dipendente.calcolaPaga(D1, 20);
 
         Dipendente D2 = new Dipendente("Matr", Livello.IMPIEGATO, Dipartimento.AMMINISTRAZIONE);
-
+        Dipendente.stampaDatiDipendente();
         Dipendente.promuovi(D2);
         Dipendente.calcolaPaga(D2, 15);
 
